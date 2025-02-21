90d3a6e147cb6336c6bae617ec542152b0a20d0b
Madhusha Prasad <madushaprasad21@gmail.com>
2025-02-21 19:34:40 +0530

    feat: User entity done

---
+++ src/main/java/com/madhusha/spring_boot_device_seller/model/User.java
@@ -0,0 +1,28 @@
+package com.madhusha.spring_boot_device_seller.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalTime;
+
+@Entity // This annotation is used to specify the class is an entity
+@Table(name = "users") // This annotation is used to specify the table name in the database
+
+public class User {
+    @Id // This annotation is used to specify the primary key of an entity
+    @GeneratedValue(strategy = GenerationType.IDENTITY)// This annotation is used to specify the primary key generation strategy
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false, length = 100) // This annotation is used to specify the column name in the database
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 100) // This annotation is used to specify the column name in the database
+    private String password;
+
+    @Column(name = "name", nullable = false, length = 100) // This annotation is used to specify the column name in the database
+    private String name;
+
+    @Column(name = "create_time", nullable = false) // This annotation is used to specify the column name in the database
+    private LocalTime createTime;
+
+    //role
+}
