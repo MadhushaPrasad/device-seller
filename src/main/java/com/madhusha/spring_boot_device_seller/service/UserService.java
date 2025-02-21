c85cf29a9eefe1d6d38d9da54e36104f91824078
Madhusha Prasad <madushaprasad21@gmail.com>
2025-02-21 23:05:43 +0530

    feat: add methods for finding user by username and changing user role in UserService

--- src/main/java/com/madhusha/spring_boot_device_seller/service/UserService.java
+++ src/main/java/com/madhusha/spring_boot_device_seller/service/UserService.java
@@ -1,8 +1,17 @@
 package com.madhusha.spring_boot_device_seller.service;
 
+import com.madhusha.spring_boot_device_seller.model.Role;
 import com.madhusha.spring_boot_device_seller.model.User;
+import jakarta.transaction.Transactional;
+
+import java.util.Optional;
 
 public interface UserService {
 
     User saveUser(User user);
+
+    Optional<User> findByUsername(String username);
+
+    @Transactional
+    void changeUserRole(String username, Role role);
 }
--- src/main/java/com/madhusha/spring_boot_device_seller/service/impl/UserServiceImpl.java
+++ src/main/java/com/madhusha/spring_boot_device_seller/service/impl/UserServiceImpl.java
@@ -4,12 +4,14 @@ import com.madhusha.spring_boot_device_seller.model.Role;
 import com.madhusha.spring_boot_device_seller.model.User;
 import com.madhusha.spring_boot_device_seller.repository.UserRepository;
 import com.madhusha.spring_boot_device_seller.service.UserService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Optional;
 
 
 @Service
@@ -26,5 +28,16 @@ public class UserServiceImpl implements UserService {
         user.setRole(Role.USER);
         user.setCreateTime(LocalTime.from(LocalDateTime.now()));
         return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Transactional // Transactional is required to update/delete data in the database
+    @Override
+    public void changeUserRole(String username, Role role) {
+        userRepository.updateUserRole(username, role);
     }
 }
