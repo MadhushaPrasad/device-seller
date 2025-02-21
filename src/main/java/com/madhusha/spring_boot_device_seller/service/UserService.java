52aaf2a6013c89155d0274db850e2e52392e9bbe
Madhusha Prasad <madushaprasad21@gmail.com>
2025-02-21 23:01:27 +0530

    feat: add UserService interface for user management operations

---
+++ src/main/java/com/madhusha/spring_boot_device_seller/service/UserService.java
@@ -0,0 +1,8 @@
+package com.madhusha.spring_boot_device_seller.service;
+
+import com.madhusha.spring_boot_device_seller.model.User;
+
+public interface UserService {
+
+    User saveUser(User user);
+}
