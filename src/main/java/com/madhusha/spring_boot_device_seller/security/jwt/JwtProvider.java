57bec860347d8e59d9b5e6e61684b7a21c75b61f
Madhusha Prasad <madushaprasad21@gmail.com>
2025-02-22 21:31:17 +0530

    feat: implement JWT provider for token generation and validation

---
+++ src/main/java/com/madhusha/spring_boot_device_seller/security/jwt/JwtProvider.java
@@ -0,0 +1,13 @@
+package com.madhusha.spring_boot_device_seller.security.jwt;
+
+import com.madhusha.spring_boot_device_seller.security.UserPrincipal;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+
+public interface JwtProvider {
+    String generateToken(UserPrincipal auth);
+
+    Authentication getAuthentication(HttpServletRequest request);
+
+    boolean isTokenValid(HttpServletRequest request);
+}
