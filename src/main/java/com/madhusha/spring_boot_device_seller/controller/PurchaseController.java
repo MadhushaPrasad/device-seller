c866039745dcbfc7aaa1fef94c1e09ac83055af6
Madhusha Prasad <madushaprasad21@gmail.com>
2025-02-24 15:00:05 +0530

    fix: update PurchaseController API endpoint from /api/purchases to /api/purchase

--- src/main/java/com/madhusha/spring_boot_device_seller/controller/PurchaseController.java
+++ src/main/java/com/madhusha/spring_boot_device_seller/controller/PurchaseController.java
@@ -10,7 +10,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/purchases")
+@RequestMapping("/api/purchase")
 public class PurchaseController {
 
     @Autowired
