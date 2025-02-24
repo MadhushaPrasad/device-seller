318348a7ca0a7c95c0f118a8a5da7f630fd1f8c7
Madhusha Prasad <madushaprasad21@gmail.com>
2025-02-24 14:54:51 +0530

    fix: update DeviceController API endpoint from /api/devices to /api/device

--- src/main/java/com/madhusha/spring_boot_device_seller/controller/DeviceController.java
+++ src/main/java/com/madhusha/spring_boot_device_seller/controller/DeviceController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/devices")
+@RequestMapping("/api/device")
 public class DeviceController {
 
     @Autowired
